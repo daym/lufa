158aa202aba4735f869b0a629ce35a524173e4fa
Danny Milosavljevic <dannym@scratchpost.org>
2014-06-01 19:20:18 +0200

    make MIDI2 a bridge

--- Demos/Device/ClassDriver/MIDI2/MIDI.c
+++ Demos/Device/ClassDriver/MIDI2/MIDI.c
@@ -34,6 +34,8 @@
 #include <avr/interrupt.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdint.h>
+typedef uint8_t uchar;
 
 #include "Descriptors.h"
 
@@ -41,6 +43,9 @@
 #include <LUFA/Drivers/USB/USB.h>
 #include <LUFA/Platform/Platform.h>
 #include "MIDI.h"
+
+#define HW_CDC_BULK_OUT_SIZE     8
+#define HW_CDC_BULK_IN_SIZE      8
 
 static USB_ClassInfo_MIDI_Device_t Keyboard_MIDI_Interface = {
 	.Config = {
@@ -58,10 +63,123 @@ static USB_ClassInfo_MIDI_Device_t Keyboard_MIDI_Interface = {
 	},
 };
 
-#define COLS 6
-static uint8_t columnPins[COLS] = {_BV(1), _BV(2), _BV(3), _BV(4), _BV(0), _BV(5)}; /* PORTC */
+/* TxLED = PD5 */
+#define LEDTx_ON  { PORTD &= ~0b00100000; }
+#define LEDTx_OFF { PORTD |=  0b00100000; }
+/* RxLED = PD4 */
+#define LEDRx_ON  { PORTD &= ~0b00010000; }
+#define LEDRx_OFF { PORTD |=  0b00010000; }
+#define    RX_SIZE        (HW_CDC_BULK_IN_SIZE)
+static uchar utxrdy = false;    /* USB Packet ready in utx_buf */
+static uchar rx_buf[RX_SIZE];   /* tempory buffer */
+static uchar utx_buf[RX_SIZE];  /* BULK_IN buffer */
 
-static uint16_t keystate[COLS] = {0};
+#define    TX_SIZE        (HW_CDC_BULK_OUT_SIZE<<2)
+#define    TX_MASK        (TX_SIZE-1)
+static uchar uwptr = 0, irptr = 0;
+static uchar tx_buf[TX_SIZE];
+
+void parseUSBMidiMessage(uchar *data, uchar len) {
+  uchar cin = (*data) & 0x0f;	/* CABLE NOを無視する */
+  uchar i;
+
+  LEDRx_ON
+
+  if (cin > 1) {		/* ignore cin == 0 and cin == 1 */
+    for (i = 1 ; i < 4 ; i++) {
+      tx_buf[uwptr++] = *(data + i);
+      uwptr &= TX_MASK;
+      if (i == 1) {
+	if ((cin == 5) || /* single byte system common */
+	    (cin == 15))  /* single byte */
+	  break;
+      }
+      if (i == 2) {
+	if ((cin == 2) ||  /* two-byte system common */
+	    (cin == 6) ||  /* system ex end with 2 bytes */
+	    (cin == 12) || /* program change */
+	    (cin == 13))   /* channel pressure */
+	  break;
+      }
+    }
+  }
+
+  LEDRx_OFF
+
+  if (len > 4) {
+    parseUSBMidiMessage(data+4, len-4);
+  }
+}
+
+uchar parseSerialMidiMessage(uchar RxByte) {
+  static uchar PC = 0;
+  static uchar SysEx = false;
+  static uchar stateTransTable[] = {
+    0, 				/* 0 dummy */
+    0,				/* 1 dummy */
+    3,				/* 2->3 NOTE OFF (3) */
+    2 | 0x80,			/* 3->2 */
+    5,				/* 4->5 NOTE ON (3) */
+    4 | 0x80,			/* 5->4 */
+    7,				/* 6->7 Polyphonic key pressure (3) */
+    6 | 0x80,			/* 7->6 */
+    9,				/* 8->9 Control Change (3) */
+    8 | 0x80,			/* 8->9 */
+    10 | 0x80,			/* 10->10 program change (2) */
+    0,				/* dummy */
+    12 | 0x80,			/* 12->12 Channel Pressure (2) */
+    0,				/* 13 dummy */
+    15,				/* 14->15 Pitch Bend (3) */
+    14 | 0x80			/* 15->14 */
+  };
+
+  LEDRx_ON
+
+  if(SysEx){  /* MIDI System Message */
+    if(RxByte == 0xf7){		/* MIDI_EndSysEx */
+      SysEx = false;
+    }
+    LEDRx_OFF
+    return false;
+  }
+  if (RxByte >= 0xF8){		/* Single Byte Message */
+    utx_buf[0] = 0x0f;
+    utx_buf[1] = RxByte;
+    utx_buf[2] = 0;
+    utx_buf[3] = 0;
+    LEDRx_OFF
+    return true;
+  }
+
+  if(RxByte > 0x7F){		/* Channel message */
+    if(RxByte == 0xf0){		/* MIDI_StartSysEx */
+      SysEx = true;
+      LEDRx_OFF
+      return false;
+    }
+    PC = 0;
+  }
+
+  if (PC == 0) {
+    PC = (((RxByte >> 4) & 0x07) + 1) * 2;
+    // conversion
+    // 0x80 -> 2, 0x90 -> 4, 0xa0 -> 6, 0xb0 -> 8, 0xc0 -> 10, 0xd0 -> 12, 0xe0 -> 14
+    rx_buf[0] = RxByte >> 4;
+    rx_buf[1] = RxByte;
+    rx_buf[3] = 0;
+  } else {
+    uchar tt = stateTransTable[PC];
+    rx_buf[(PC & 1) + 2] = RxByte;
+    PC = tt & 0x0f;
+    if ((tt & 0x80) != 0) {
+      memcpy(utx_buf, rx_buf, 4);
+      LEDRx_OFF
+      return true;
+    }
+  }
+  LEDRx_OFF
+  return false;
+}
 
 static void sendMIDIPacket(uint8_t Channel, uint8_t MIDICommand, uint8_t MIDIPitch) {
 	if (MIDICommand) {
@@ -76,52 +194,6 @@ static void sendMIDIPacket(uint8_t Channel, uint8_t MIDICommand, uint8_t MIDIPit
 	}
 }
 static uint8_t Channel = MIDI_CHANNEL(1);
-
-/* PORTC is the octave selector, and driver.
-   PD0 and PD1 are free.
-   PIND, PINB are the (half)tone selectors, 12 of them, pins 2,3,4,5,6,7,8,9,10,11,12,13. */
-static void Keyboard_Scan(void) {
-	for(uint8_t c = 0; c < COLS; ++c) {
-#if 0
-		if(columnPins[c] == _BV(0)) /* no idea why I'm not allowed to do that. USB device vanishes when I try. */
-			continue;
-#endif
-		uint16_t prevstate, newstate;
-		DDRC |= columnPins[c]; /* set octave output */
-		/* breaks; am not allowed to do that! */
-		PORTC &=~ columnPins[c]; /* pulse column */
-#if 0
-		prevstate = keystate[c];
-		newstate = ((uint16_t) PIND | ((uint16_t) PINB << 8)) >> 2;
-		if(prevstate != newstate) { /* send MIDI event */
-			uint8_t note;
-			uint16_t changes = newstate ^ prevstate;
-			for(note = 1; note < 12; ++note) { /* FIXME note 0 seems to change continously */
-				if((changes & (1 << note)) != 0) {
-					sendMIDIPacket(Channel, (newstate & _BV(note)) == 0 ? MIDI_COMMAND_NOTE_ON : MIDI_COMMAND_NOTE_OFF, 36 + c * 12 + note);
-				}
-			}
-			keystate[c] = newstate;
-		}
-#endif
-		/* TODO debounce */
-		PORTC |= columnPins[c]; /* end pulse */
-		DDRC &=~ columnPins[c]; /* end pulse */ /* breaks it */
-	}
-}
-
-static void Keyboard_Init(void) {
-	DDRB = 0; /* set input */
-	DDRD &= 3; /* set input */ /* don't touch RX/TX */
-	PORTB = 0xFF; /* activate pull-ups */
-	PORTD |= 0xFF &~ 3; /* activate pull-ups */ /* don't touch RX/TX */
-	DDRC = 0; /* input for now */
-	PORTC = 0xFF; /* pullup for now */
-	uint8_t c;
-	for(c = 0; c < COLS; ++c)
-		keystate[c] = 0xFF;
-}
-
 
 /** Configures the board hardware and chip peripherals for the demo's functionality. */
 void SetupHardware(void) {
@@ -144,9 +216,12 @@ void SetupHardware(void) {
 	PMIC.CTRL = PMIC_LOLVLEN_bm | PMIC_MEDLVLEN_bm | PMIC_HILVLEN_bm;
 #endif
 
-	/* Hardware Initialization */
+	DDRD |= 0b00110000; /* PD5,4 = OUTPUT for LED */
+	PORTD |= 0b00110000;          /* turn off LED (PD4, PD5) */
+	/* FIXME UBRR1H */
+	UBRR1L = 31;                  /* 31250Hz at 16MHz clock */
+	UCSR1B = (1<<RXEN1) | (1<<TXEN1);
 	USB_Init();
-	Keyboard_Init();
 }
 
 /** Event handler for the library USB Connection event. */
@@ -172,21 +247,36 @@ void EVENT_USB_Device_ControlRequest(void) {
  *  setup of all components and the main program loop.
  */
 int main(void) {
-	SetupHardware();
-	GlobalInterruptEnable();
-	for (;;) {
-		Keyboard_Scan();
-		MIDI_EventPacket_t ReceivedMIDIEvent;
-		while (MIDI_Device_ReceiveEventPacket(&Keyboard_MIDI_Interface, &ReceivedMIDIEvent))
-		{
-			if ((ReceivedMIDIEvent.Event == MIDI_EVENT(0, MIDI_COMMAND_NOTE_ON)) && (ReceivedMIDIEvent.Data3 > 0))
-			  ; /*LEDs_SetAllLEDs(ReceivedMIDIEvent.Data2 > 64 ? LEDS_LED1 : LEDS_LED2);*/
-			else
-			 ; /* LEDs_SetAllLEDs(LEDS_NO_LEDS);*/
-		}
+  SetupHardware();
+  GlobalInterruptEnable();
+  for (;;){ 
+    /* receive from Serial MIDI line */
+    if( UCSR1A & (1<<RXC1)) {
+      utxrdy |= parseSerialMidiMessage(UDR1);
+    }
 
-		MIDI_Device_USBTask(&Keyboard_MIDI_Interface);
-		USB_USBTask();
-	}
-	return 0;
+    /* send packets to USB MIDI */
+    if( utxrdy ) {
+      MIDI_Device_SendEventPacket(&Keyboard_MIDI_Interface, (MIDI_EventPacket_t *)&utx_buf);
+      MIDI_Device_Flush(&Keyboard_MIDI_Interface);
+      utxrdy = false;
+    }
+
+    /* receive from USB MIDI */
+    MIDI_EventPacket_t ReceivedMIDIEvent;
+    while (MIDI_Device_ReceiveEventPacket(&Keyboard_MIDI_Interface, &ReceivedMIDIEvent)) {
+      /* for each MIDI packet w/ 4 bytes */
+      parseUSBMidiMessage((uchar *)&ReceivedMIDIEvent, 4);
+    }
+      
+    /* send to Serial MIDI line  */
+    if( (UCSR1A & (1<<UDRE1)) && uwptr!=irptr ) {
+      UDR1 = tx_buf[irptr++];
+      irptr &= TX_MASK;
+    }
+
+    MIDI_Device_USBTask(&Keyboard_MIDI_Interface);
+    USB_USBTask();
+  }
+  return 0;
 }
